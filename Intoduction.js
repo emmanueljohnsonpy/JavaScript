af579297c0014e17496e0155fb5c7c37a323b762
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2024-12-06 19:26:48 +0530

    javascript w3schools started

---
+++ Intoduction.js
@@ -0,0 +1,14 @@
+// JavaScript Can Change HTML Content
+
+// JavaScript Can Change HTML Attribute 
+
+// JavaScript Can Change HTML Styles (CSS)
+
+// JavaScript Can Hide HTML Elements
+
+// JavaScript Can Show HTML Elements
+
+/* Writing into an HTML element, using innerHTML.
+Writing into the HTML output using document.write().
+Writing into an alert box, using window.alert().
+Writing into the browser console, using console.log(). */
