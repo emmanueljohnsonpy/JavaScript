689d81604a67e1cf34a6af35fdbe02c7abc66f3b
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2024-12-09 19:08:33 +0530

    loops

---
+++ basic.js
@@ -0,0 +1,83 @@
+
+
+// Conditional Statements
+
+
+/* num = -2
+if (num<0){
+    console.log("Number is negative")
+}else if(num>0){
+    console.log("Number is positive")
+}else{
+    console.log("Number is Zero")
+} */
+
+
+/* num=7
+switch (num){
+    case 1:
+        console.log("One")
+        break
+    case 2:
+        console.log("Two")
+        break
+    case 3:
+        console.log("Three")
+        break
+    default:
+        console.log("Not Found")
+} */
+
+
+// Loop
+
+
+/* for (let i=0; i<=5; i++){
+    console.log(i)
+} */
+
+
+/* const person = {fname: "John", lname: "Doe", age: 30}
+for (let x in person){
+    console.log(person[x])
+} */
+
+/* const number = [1, 2, 3, 4, 5]
+for (let x in number){
+    console.log(number[x])
+} */
+
+/* const numbers=[1, 2, 3, 4, 5]
+let txt=""
+numbers.forEach(my_func)
+function my_func(value, index, array){
+    txt+=value
+}
+console.log(txt) */
+
+/* let numbers = ["Apple", "Orange", "Banana", "Strawberry", "Grapes"]
+for (let x of numbers){
+    console.log(x)
+} */
+
+/* let my_str="hai how are you"
+for (let x of my_str){
+    process.stdout.write(x)
+} */
+
+
+/* i=1
+while (i<=10){
+    console.log(i)
+    i++
+} */
+
+
+/* i=0
+do {
+    console.log(i)
+    i++
+}
+while (i>10) */
+
+
